c9b7efcacaab5d5787b76ea409b7800459dac851
Orlando Lucero <orlandolucero@Orlandos-Mac-mini.local>
2024-12-16 01:14:42 -0600

    UPDATE: Se agregaron funciones en chats service para fijar y eliminar chats

--- lib/services/chats_service.dart
+++ lib/services/chats_service.dart
@@ -1,13 +1,18 @@
-import 'package:chat_app/models/chats_response.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:chat_app/global/environment.dart';
 
 import 'package:chat_app/services/auth_service.dart';
+
 import 'package:chat_app/models/chat.dart';
+import 'package:chat_app/models/chats_response.dart';
 //import 'package:chat_app/models/users_response.dart';
 
-class ChatsService {
+class ChatsService with ChangeNotifier {
   Future<List<Chat>> getChats() async {
     try {
       final res = await http.get(
@@ -25,6 +30,62 @@ class ChatsService {
       return chatsResponse.chats;
     } catch (error) {
       return [];
+    }
+  }
+
+  Future<String> pinChat(String toUserId) async {
+    try {
+      final res = await http.post(
+        Uri.parse('${Environment.apiUrl}/pin-chat'),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-token': await AuthService.getToken(),
+        },
+        body: jsonEncode({
+          "toUserId": toUserId
+        }),
+      );
+
+      print(res.body);
+      if (res.statusCode == 200) {
+        return '';
+      } else {
+        return 'Unexpected error: ${res.statusCode}';
+      }
+    } on SocketException {
+      return 'Connection was refused';
+    } on HttpException {
+      return 'There\'s an error while connecting with the server';
+    } catch (error) {
+      return 'Unexpected error: $error';
+    }
+  }
+
+  Future<String> deleteChat(String toUserId) async {
+    try {
+      final res = await http.post(
+        Uri.parse('${Environment.apiUrl}/delete-chat'),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-token': await AuthService.getToken(),
+        },
+        body: jsonEncode({
+          "toUserId": toUserId
+        }),
+      );
+
+      print(res.body);
+      if (res.statusCode == 200) {
+        return '';
+      } else {
+        return 'Unexpected error: ${res.statusCode}';
+      }
+    } on SocketException {
+      return 'Connection was refused';
+    } on HttpException {
+      return 'There\'s an error while connecting with the server';
+    } catch (error) {
+      return 'Unexpected error: $error';
     }
   }
 }
